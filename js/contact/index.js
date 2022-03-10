1ccdc4c213cf62f99ff59aca5c223438020dac73
Seitek2002 <seitek_2002@bk.ru>
2022-03-10 12:57:23 +0600

    qwerty

--- js/contact/index.js
+++ js/contact/index.js
@@ -1,9 +1,17 @@
 const contacts_info__description = document.querySelector(".contacts-info__description");
+const contacts_info__item = document.querySelector(".contacts-info__item");
 
 const getContactResponse = async () => {
   const response = await fetch("https://intuit-iaeee-back.herokuapp.com/contact");
   const data = await response.json();
-
+  contacts_info__item.innerHTML += `
+    <a href="tel:${data.contacts_info__item.phone}" class="contacts-info__phone">
+        ${data.contacts_info__item.phone}
+    </a>
+    <a href="mailto:${data.contacts_info__item.email}" class="contacts-info__email">
+        ${data.contacts_info__item.email}
+    </a>
+  `;
   contacts_info__description.innerHTML += data.contacts__info__description;
 };
 
