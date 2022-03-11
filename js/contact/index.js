baf0f316175ad794a2dcb8bfe010878b78bd7d98
Seitek2002 <seitek_2002@bk.ru>
2022-03-12 00:54:53 +0600

    naodemailer

--- js/contact/index.js
+++ js/contact/index.js
@@ -1,8 +1,12 @@
-const contacts_info__description = document.querySelector(".contacts-info__description");
+const contacts_info__description = document.querySelector(
+  ".contacts-info__description"
+);
 const contacts_info__item = document.querySelector(".contacts-info__item");
 
 const getContactResponse = async () => {
-  const response = await fetch("https://intuit-iaeee-back.herokuapp.com/contact");
+  const response = await fetch(
+    "https://intuit-iaeee-back.herokuapp.com/contact"
+  );
   const data = await response.json();
   contacts_info__description.innerHTML += data.contacts__info__description;
   contacts_info__item.innerHTML += `
@@ -16,3 +20,35 @@ const getContactResponse = async () => {
 };
 
 getContactResponse();
+
+// const name = document.querySelector(".name");
+// const phone = document.querySelector(".phone");
+// const email = document.querySelector(".email");
+// const product = document.querySelector(".product");
+// const description = document.querySelector(".contacts__textarea");
+const btnSubmit = document.querySelector(".modal__btn");
+// https://intuit-iaeee-back.herokuapp.com/api/auth/registration
+// const postMailResponse = async () => {
+//   const response = await fetch(
+//     "http://localhost:8080/api/auth/registration",
+//     {
+//       method: "POST",
+//       body: JSON.stringify({
+//         name: document.querySelector(".name"),
+//         phone: document.querySelector(".phone"),
+//         email: document.querySelector(".email"),
+//         product: document.querySelector(".product"),
+//         description: document.querySelector(".contacts__textarea"),
+//       }),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   );
+//   const data = await response.json();
+// };
+
+// btnSubmit.addEventListener("click", () => {
+//   postMailResponse();
+//   console.log("qwertyu");
+// });
--- js/contacts.js
+++ js/contacts.js
@@ -12,18 +12,38 @@ feedBack.addEventListener('click', () => {
 
   modalForm.addEventListener('submit', function (e) {
     e.preventDefault();
-
     const name 				= modalForm.name.value;
     const phone 			= modalForm.phone.value;
     const email 			= modalForm.email.value;
     const product 		= modalForm.product.value;
     const description = modalForm.description.value;
 
+    const postMailResponse = async () => {
+      const response = await fetch(
+        "http://localhost:8080/api/auth/registration",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            name: modalForm.name.value,
+            phone: modalForm.phone.value,
+            email: modalForm.email.value,
+            product: modalForm.product.value,
+            description: modalForm.description.value,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+    };
+
     if (name && phone && email && product && description !== '') {
       if (modalCheckbox.checked) {
         modalBtn.addEventListener('click', function () {
           modal.style.display = 'none';
           modalThanks.style.display = 'flex';
+          postMailResponse()
         });
       } else {
         alert('Пожалуйста, примите политику конфиденциальности!!!');
