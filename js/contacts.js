e9d92557cf00555e14bf6fe1869639c9361748bf
Seitek2002 <seitek_2002@bk.ru>
2022-03-12 01:01:09 +0600

    test

--- js/contacts.js
+++ js/contacts.js
@@ -1,21 +1,21 @@
-const feedBack 				= document.querySelector('.contacts-info__btn');
-const modal 					= document.querySelector('.modal');
-const modalForm 			= document.querySelector('.modal__form');
-const modalClose 			= document.querySelector('.modal__close');
-const modalBtn 				= document.querySelector('.modal__btn');
-const modalThanks 		= document.querySelector('.modal-thanks');
-const modalCheckbox 	= document.querySelector('#modal-checkbox');
-const modalBack 			= document.querySelector('.modal-thanks__btn');
+const feedBack = document.querySelector(".contacts-info__btn");
+const modal = document.querySelector(".modal");
+const modalForm = document.querySelector(".modal__form");
+const modalClose = document.querySelector(".modal__close");
+const modalBtn = document.querySelector(".modal__btn");
+const modalThanks = document.querySelector(".modal-thanks");
+const modalCheckbox = document.querySelector("#modal-checkbox");
+const modalBack = document.querySelector(".modal-thanks__btn");
 
-feedBack.addEventListener('click', () => {
-  modal.style.display = 'flex';
+feedBack.addEventListener("click", () => {
+  modal.style.display = "flex";
 
-  modalForm.addEventListener('submit', function (e) {
+  modalForm.addEventListener("submit", function (e) {
     e.preventDefault();
-    const name 				= modalForm.name.value;
-    const phone 			= modalForm.phone.value;
-    const email 			= modalForm.email.value;
-    const product 		= modalForm.product.value;
+    const name = modalForm.name.value;
+    const phone = modalForm.phone.value;
+    const email = modalForm.email.value;
+    const product = modalForm.product.value;
     const description = modalForm.description.value;
 
     const postMailResponse = async () => {
@@ -28,7 +28,13 @@ feedBack.addEventListener('click', () => {
             phone: modalForm.phone.value,
             email: modalForm.email.value,
             product: modalForm.product.value,
-            description: modalForm.description.value,
+            description: `${
+              (modalForm.name.value,
+              modalForm.phone.value,
+              modalForm.email.value,
+              modalForm.product.value,
+              modalForm.description.value)
+            }`,
           }),
           headers: {
             "Content-Type": "application/json",
@@ -38,37 +44,37 @@ feedBack.addEventListener('click', () => {
       const data = await response.json();
     };
 
-    if (name && phone && email && product && description !== '') {
+    if (name && phone && email && product && description !== "") {
       if (modalCheckbox.checked) {
-        modalBtn.addEventListener('click', function () {
-          modal.style.display = 'none';
-          modalThanks.style.display = 'flex';
-          postMailResponse()
+        modalBtn.addEventListener("click", function () {
+          modal.style.display = "none";
+          modalThanks.style.display = "flex";
+          postMailResponse();
         });
       } else {
-        alert('Пожалуйста, примите политику конфиденциальности!!!');
+        alert("Пожалуйста, примите политику конфиденциальности!!!");
       }
     } else {
-      alert('Пожалуйста, заполните ВСЕ поля!!!');
+      alert("Пожалуйста, заполните ВСЕ поля!!!");
     }
   });
-  modalBack.addEventListener('click', function (e) {
-    modalThanks.style.display = 'none';
+  modalBack.addEventListener("click", function (e) {
+    modalThanks.style.display = "none";
 
-    modalForm.name.value = '';
-    modalForm.phone.value = '';
-    modalForm.email.value = '';
-    modalForm.product.value = '';
-    modalForm.description.value = '';
+    modalForm.name.value = "";
+    modalForm.phone.value = "";
+    modalForm.email.value = "";
+    modalForm.product.value = "";
+    modalForm.description.value = "";
   });
 });
 
-modalClose.addEventListener('click', function () {
-  modal.style.display = 'none';
+modalClose.addEventListener("click", function () {
+  modal.style.display = "none";
 });
 
-window.addEventListener('keypress', function (e) {
-  if (e.key === 'Enter') {
-    modal.style.display = 'none';
+window.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    modal.style.display = "none";
   }
 });
